db42be65e5d71aac4cab98b3e2159991c9f9120c
Dan Gelok <dgelok@gmail.com>
2020-10-07 19:37:44 -0500

    working updates

--- first_app/views.py
+++ first_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index (request):
-    my_dict = {'insert_me': "Hello, I'm from first_app/index.html"}
+    my_dict = {'insert_me': "Hello, I'm from first_app/index.html. I'm passed as a value of a key in an object."}
     return render(request, 'first_app/index.html', context=my_dict)
 
 # def about (request):
