29082066f2b1b76067d74902bbf5e5661183db4e
Dan Gelok <dgelok@gmail.com>
2020-10-07 20:38:37 -0500

    hiding key

--- newproject/settings.py
+++ newproject/settings.py
@@ -23,7 +23,6 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '4s=srf%zd0ixs*-%8ilq53tpd=#z-6sg35lg^#s*%hv4b2o=13'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
