e883b69f2cd393526e8b70341401d0d4925c3f34
Dan Gelok <dgelok@gmail.com>
2020-10-08 10:31:35 -0500

    adding models and migrations

--- newproject/urls.py
+++ newproject/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name="about"),
     path('contact/', views.contact, name="contact"),
+    path('admin/', admin.site.urls),
 ]
