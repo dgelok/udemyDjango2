2fffe1208b830679671ed179d941ec787216024b
Dan Gelok <dgelok@gmail.com>
2020-10-09 18:47:55 -0500

    MTV paradigm

--- first_app/views.py
+++ first_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
 
 def index (request):
     my_dict = {'insert_me': "Hello, I'm from first_app/index.html. I'm passed as a value of a key in an object."}
-    return render(request, 'first_app/index.html', context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+    return render(request, 'first_app/index.html', context=date_dict)
 
 def about (request):
     return HttpResponse("Here's our about page")
