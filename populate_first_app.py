a10800451907ce2e73ce2a7d702335ad0ecae477
Dan Gelok <dgelok@gmail.com>
2020-10-10 17:11:29 -0500

    update in AppThree, mostly done

--- populate_first_app.py
+++ populate_first_app.py
@@ -42,7 +42,7 @@ def populate(N=5):
         acc_rec = AccessRecord.objects.get_or_create(name=webpg,date=fake_date)[0]
 
 if __name__ == '__main__':
-    print("populating script!")
+    print("populating script!") 
     populate(20)
     print("success!")
 
